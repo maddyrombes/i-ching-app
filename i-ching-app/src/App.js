85b69a0e50f10fd799bab51cb55830bc57bb3e77
Madeline Rombes <madelinerombes@gmail.com>
2019-07-28 12:13:21 -0400

    basic reading rendered

--- i-ching-app/src/App.js
+++ i-ching-app/src/App.js
@@ -1,11 +1,68 @@
 import React from 'react';
 import './App.css';
+const iChing = require('i-ching');
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      question: "",
+      reading: {
+        hex_number: 0,
+        hex_character: "",
+        hex_names: ""
+      }
+    }
+  }
+
+  getReading = () => {
+    const reading = iChing.ask(this.state.question);
+
+    this.setState({
+      reading: {
+        hex_number: reading.hexagram.number,
+        hex_character: reading.hex_character,
+        hex_names: reading.hexagram.names.join(', ')
+      }
+    })
+
+
+    if (reading.change) {
+      console.log('changing lines: %j', reading.change.changingLines);
+      console.log('change to hexagram: %d %s %s',
+        reading.change.to.number,
+        reading.change.to.character,
+        reading.change.to.names.join(', '));
+
+    } else {
+      console.log('no changing lines');
+    }
+  }
+
+  handleFormChange = (e) => {
+    this.setState({ question: e.target.value })
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.getReading()
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        <form onSubmit={this.handleFormSubmit}>
+          What is your question?
+            <input type="text" value={this.state.question} onChange={this.handleFormChange} />
+        </form>
+        <p>Hexagram number: {this.state.reading.hex_number}</p>
+        <p>Hexagram character: {this.state.reading.hex_character}</p>
+        <p>Hexagram names: {this.state.reading.hex_names}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
